acb96062c7a7d04a945e1c99cead672c7a1159f7
Muhammad Ali Khan <ak098boy@gmail.com>
2024-09-28 16:59:19 +0500

    Add files via upload

---
+++ circQ.java
@@ -0,0 +1,106 @@
+
+    import java.util.Scanner;
+    class circQ {	// start of circular queue class
+
+        static int cqueue[] = new int[20]; //declaration of global variables
+        static int max=5;
+        static int rear=-1,front=-1,item;
+
+        public static void main(String arg[]) { // start of main()
+            char choice=' ';
+            Scanner input = new Scanner(System.in);
+
+            while(true) { // start of while
+
+                System.out.println("1 : Insert In Circular Queue (Insert)");
+                System.out.println("2 : Delete From Circular Queue (Front)");
+                System.out.println("3 : Display Circular Queue");
+                System.out.println("4 : Exit\n");
+                System.out.print("Enter Choice...");
+
+                choice=input.next().charAt(0);
+
+                switch(choice) { //start of switch statement
+
+                    case '1': System.out.print("\nEnter value into Queue... ");
+                        item=input.nextInt();
+                        cqinsert(item);
+                        break;
+
+                    case '2': cqdelete();
+                        break;
+
+                    case '3': display();
+                        break;
+
+                    case '4': System.exit(0);
+                        break;
+
+                } //end switch statement
+            } //end while
+        }//end main
+
+        public static void cqinsert(int n) { // start of push()
+
+            if ((front==0 && rear==max-1) || front==rear+1) {
+                System.out.println("\n Circular Queue is Full\n");
+            }
+            else {
+                if (front==-1){
+                    front=rear=0;
+                }
+                else{
+                    if (rear==max-1)
+                        rear=0;
+                    else
+                        rear++;
+                } //endif
+                cqueue[rear]=n;
+                System.out.println("Item Inserted in circular Queue");
+            } //endif
+
+        }//end circular Queue insert
+
+        public static void cqdelete() { //start of qdelete()
+            if (front==-1)  {
+                System.out.println("circular Queue Is Empty\n");
+            }
+            else {
+                item=cqueue[front];
+                System.out.println("\nThe deleted Item..."+item);
+                if (front==rear)
+                {
+                    front=-1;
+                    rear=-1;
+                }
+                else
+                {
+                    if (front==max-1)
+                        front=0;
+                    else
+                        front++;
+                } //endif
+            } // endif
+
+        } //end cqdelete
+
+        public static void display() { // start of display()
+            if (rear!=-1) {
+                int i= front;
+                while(i != rear   ) {
+                    System.out.println(cqueue[i]);
+                    i++;
+                            if(i == max )
+                                i=0;
+                            if(i == rear)
+                                System.out.println(cqueue[i]);
+                }
+            }
+            else
+                System.out.println("\nNO Value in Cricular Queue\n");
+
+        }//end display
+
+    } //end class circular queue
+
+
---
+++ deQue.java
@@ -0,0 +1,125 @@
+import java.util.Scanner;
+public class deQue {
+    final static int[] data = new int[20];
+    static int left = 0;
+    static int right = -1;
+    static int count=0;
+    static int max = 5;
+
+    public static void insertLeft(int item){
+        if(count == max)
+            System.out.println("Queue is full");
+        else {
+            left--;
+            if(left < 0)
+                left = max-1;
+
+            data[left] = item;
+            count++;
+        }
+    }
+    public static void insertRight(int item){
+        if(count == max)
+            System.out.println("Queue is full");
+        else{
+            right++;
+            if(right >=max)
+                right = 0;
+
+            data[right] = item;
+            count++;
+        }
+    }
+    public static void delLeft(){
+        if(count == 0)
+            System.out.println("Queue is empty");
+        else {
+            int item;
+            item = data[left];
+            System.out.println(item);
+            left++;
+            if (left > max)
+                left = 0;
+            count--;
+        }
+    }
+    public static void delRight(){
+        if(count == 0)
+            System.out.println("Queue is empty");
+        else {
+            int item;
+            item = data[right];
+            System.out.println(item);
+            right--;
+            if(right<0)
+                right = max-1;
+            count--;
+        }
+    }
+    public static void display(){
+        if(count ==0)
+            System.out.println("Queue is empty");
+        else{
+            int i = left;
+            System.out.print("Queue has: ");
+            for(int j=0; j<count;j++){
+                System.out.print(data[i] + " ");
+                i++;
+                if(i > max-1)
+                    i = 0;
+            }
+        }
+    }
+    public static void displayOperators(){
+        System.out.println("\nLeft: " + left);
+        System.out.println("Right: " + right);
+    }
+
+    public static void main(String[] args) {
+        char choice = ' ';
+        Scanner input = new Scanner(System.in);
+        int item;
+        while(true){
+            System.out.println("\n1: Insert from Right");
+            System.out.println("2: Insert from Left");
+            System.out.println("3: Delete from Right");
+            System.out.println("4: Delete from Left");
+            System.out.println("5: Display");
+            System.out.println("6: Exit");
+
+            System.out.print("\nYour Choice: ");
+            choice = input.next().charAt(0);
+
+            switch (choice) {
+                case '1':
+                    System.out.print("Enter value to insert from right: ");
+                    item = input.nextInt();
+                    insertRight(item);
+                    break;
+                case '2':
+                    System.out.print("Enter value to insert from left: ");
+                    item = input.nextInt();
+                    insertLeft(item);
+                    break;
+                case '3':
+                    System.out.println("Delete from right...");
+                    delRight();
+                    break;
+                case '4':
+                    System.out.println("Deleting from left...");
+                    delLeft();
+                    break;
+                case '5':
+                    display();
+                    break;
+                case '6':
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Enter a valid choice!");
+                    break;
+            }
+            displayOperators();
+        }
+    }
+}
