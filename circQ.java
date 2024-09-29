aee123818d52c4b6365cd126a0a318e6e514fe9c
Muhammad Ali Khan <ak098boy@gmail.com>
2024-09-29 23:42:22 +0500

    circQ.java

--- circQ.java
+++ circQ.java
@@ -1,112 +1,101 @@
+import java.util.Scanner;
 
-    import java.util.Scanner;
-    class circQ {	// start of circular queue class
+public class circQ{
+    static int[] cq = new int[20];
+    static int max = 5;
+    static int front = -1, rear = -1;
+    static int item;
 
-        static int cqueue[] = new int[20]; //declaration of global variables
-        static int max=5;
-        static int rear=-1,front=-1,item;
-
-        public static void main(String arg[]) { // start of main()
-            char choice=' ';
-            Scanner input = new Scanner(System.in);
-
-            while(true) { // start of while
-
-                System.out.println("\n1 : Insert In Circular Queue (Insert)");
-                System.out.println("2 : Delete From Circular Queue (Front)");
-                System.out.println("3 : Display Circular Queue");
-                System.out.println("4 : Exit\n");
-                System.out.print("Enter Choice...");
-
-                choice=input.next().charAt(0);
-
-                switch(choice) { //start of switch statement
-
-                    case '1': System.out.print("\nEnter value into Queue... ");
-                        item=input.nextInt();
-                        cqinsert(item);
-                        break;
-
-                    case '2': cqdelete();
-                        break;
-
-                    case '3': display();
-                        break;
-
-                    case '4': System.exit(0);
-                        break;
-
-                } //end switch statement
-                displayPointers();
-            } //end while
-        }//end main
-
-        public static void cqinsert(int n) { // start of push()
-
-            if ((front==0 && rear==max-1) || front==rear+1) {
-                System.out.println("\n Circular Queue is Full\n");
+    public static void cqInsert(int item){
+        if(front == 0 && rear == max-1 || front == rear + 1)
+            System.out.println("Queue is overflow...");
+        else {
+            if(front == -1){
+                front = 0;
+                rear = 0;
             }
             else {
-                if (front==-1){
-                    front=rear=0;
-                }
-                else{
-                    if (rear==max-1)
-                        rear=0;
-                    else
-                        rear++;
-                } //endif
-                cqueue[rear]=n;
-                System.out.println("Item Inserted in circular Queue");
-            } //endif
-
-        }//end circular Queue insert
-
-        public static void cqdelete() { //start of qdelete()
-            if (front==-1)  {
-                System.out.println("circular Queue Is Empty\n");
-            }
-            else {
-                item=cqueue[front];
-                System.out.println("\nThe deleted Item..."+item);
-                if (front==rear)
-                {
-                    front=-1;
-                    rear=-1;
-                }
+                if (rear == max-1)
+                    rear = 0;
                 else
-                {
-                    if (front==max-1)
-                        front=0;
-                    else
-                        front++;
-                } //endif
-            } // endif
+                    rear = rear + 1;
+            }
+            cq[rear] = item;
+            System.out.println("\nItem inserted");
+        }
+    }
+    public static void cqDelete(){
+        if(front == -1)
+            System.out.println("Queue is underflow...");
+        else {
+            int item = cq[front];
+            System.out.println("Item deleted is: " + item);
+            if (front == rear)
+                front = rear = -1;
+            else{
+                if(front == max-1)
+                    front = 0;
+                else
+                    front = front + 1;
+            }
+        }
+    }
+    public static void cqDisplay(){
+        if(rear != -1){
+            int num;
+            num = front;
+            System.out.println("Queue: ");
+            while(true){
+                System.out.print(cq[num] + ", ");
+                if(num == rear){
+                    break;
+                }
 
-        } //end cqdelete
-
-        public static void display() { // start of display()
-            if (rear!=-1) {
-                int i= front;
-                System.out.print("Queue contains: ");
-                while(i != rear   ) {
-                    System.out.print(cqueue[i] + " ");
-                    i++;
-                            if(i == max )
-                                i=0;
-                            if(i == rear)
-                                System.out.println(cqueue[i]);
+                num++;
+                if(num == max)
+                    num =0;
                 }
             }
-            else
-                System.out.println("\nNO Value in Circular Queue\n");
+        else
+            System.out.println("Queue is underflow...");
+        }
+    public static void displayPointers(){
+        System.out.println("\nFront: " + front + " Rear: " + rear);
+    }
 
-        }//end display
-        public static void displayPointers(){
-            System.out.println("\nFront: " + front);
-            System.out.println("Rear: " + rear);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char choice = ' ';
+
+        while(true){
+            System.out.println("\n1: Insert in Circular Queue");
+            System.out.println("2: Delete in Circular Queue");
+            System.out.println("3: Display Circular Queue");
+            System.out.println("4: Exit");
+            System.out.print("\nEnter your choice: ");
+            choice = input.next().charAt(0);
+
+            switch (choice){
+                case '1':
+                    System.out.print("Enter the number to insert: ");
+                    int item = input.nextInt();
+                    cqInsert(item);
+                    break;
+                case '2':
+                    cqDelete();
+                    break;
+                case '3':
+                    cqDisplay();
+                    break;
+                case '4':
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Enter valid choice!");
+                    break;
+            }
+            displayPointers();
         }
 
-    } //end class circular queue
-
-
+    }
+}
