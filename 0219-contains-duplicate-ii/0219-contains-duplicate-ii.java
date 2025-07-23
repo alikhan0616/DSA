102b759f143b665a6df2cec07a80875eff9e7d71
Muhammad Ali Khan <ak098boy@gmail.com>
2025-07-23 21:53:34 +0500

    Time: 20 ms (90.89%), Space: 59.9 MB (44.29%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+       Map<Integer, Integer> hm = new HashMap<>();
+       for(int i = 0 ; i < nums.length ; i++){
+        if(hm.containsKey(nums[i]) && Math.abs(hm.get(nums[i]) - i) <= k){
+            return true;
+        } else {
+            hm.put(nums[i], i);
+        }
+       }
+       return false;
+    }
+}
