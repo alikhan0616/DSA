8ac01340182f445adeb62a8947ab0ec3527d7844
Muhammad Ali Khan <ak098boy@gmail.com>
2025-07-31 10:29:19 +0500

    Time: 0 ms (100%), Space: 42.2 MB (86.11%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if(nums.length == 1) return 0;
+        if(nums[0] > nums[1]) return 0;
+        if(nums[nums.length - 1] > nums[nums.length - 2]) return nums.length - 1;
+        int low = 1, high = nums.length - 2;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid;
+            if(nums[mid] < nums[mid - 1]){
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
