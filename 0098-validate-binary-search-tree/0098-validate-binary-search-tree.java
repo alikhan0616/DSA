e28ac972de0f4bbbeed9ea51016fd0ecadaa47e9
Muhammad Ali Khan <ak098boy@gmail.com>
2025-11-25 18:34:38 +0500

    Time: 0 ms (100%), Space: 45 MB (24.23%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,15 +15,14 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return valid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+      return valid(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
-    private boolean valid(TreeNode root, long left, long right){
-        if(root == null){
-            return true;
-        }
-        if(root.val <= left || root.val >= right){
-            return false;
-        }
+    private boolean valid(TreeNode root, long left, long right ){
+        if(root == null) return true;
+
+        if(!(left < root.val && root.val < right)) return false;
+
         return valid(root.left, left, root.val) && valid(root.right, root.val, right);
+
     }
 }
