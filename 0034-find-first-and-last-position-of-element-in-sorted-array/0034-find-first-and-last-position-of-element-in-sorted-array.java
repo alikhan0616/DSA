29b0e59b0d0a75be15f5065d3cbe651fc42a6b5d
Muhammad Ali Khan <ak098boy@gmail.com>
2026-02-09 17:34:36 +0500

    Time: 0 ms (100%), Space: 48.3 MB (26.36%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -4,33 +4,32 @@ class Solution {
         int right = findRightBound(nums, target);
         return new int[]{left, right};
     }
-    private int findLeftBound(int[]nums, int target){
-        int index = -1, low = 0, high = nums.length - 1;
-        while( low <= high ){
-            int mid = low + (high - low) / 2;
+    private int findLeftBound(int[] nums, int target){
+        int index = -1, left = 0, right = nums.length - 1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
             if(nums[mid] == target){
                 index = mid;
-                high = mid - 1;
-            } else if(nums[mid] < target){
-                low = mid + 1;
+                right = mid - 1;
+            } else if (nums[mid] > target){
+                right = mid - 1;
             } else {
-                high = mid - 1;
+                left = mid + 1;
             }
         }
         return index;
     }
-
     private int findRightBound(int[] nums, int target){
-        int index = -1, low = 0, high = nums.length - 1;
-        while(low <= high){
-            int mid =  low + (high - low) / 2;
+        int index = -1, left = 0, right = nums.length - 1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
             if(nums[mid] == target){
                 index = mid;
-                low = mid + 1;
-            } else if (nums[mid] < target){
-                low = mid + 1;
+                left = mid + 1;
+            } else if (nums[mid] > target){
+                right = mid - 1;
             } else {
-                high = mid - 1;
+                left = mid + 1;
             }
         }
         return index;
